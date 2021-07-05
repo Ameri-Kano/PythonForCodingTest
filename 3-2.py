d90a6d1b7847a38da1b530ebb0ffd8c8a871e9ce
Ameri-Kano <sanar0213@naver.com>
2021-07-05 22:38:24 +0900

    큰 수의 법칙(그리디)

--- 3-2.py
+++ 3-2.py
@@ -2,20 +2,25 @@ n, m, k = map(int, input().split())
 l = list(map(int, input().split()))
 
 l.sort(reverse=True)
-
 # 가장 큰 수를 k번, 그 다음으로 큰 수를 1번 더함
 # 이를 반복하여 총 m번 더하면 가장 큰 수가 됨
-a = l[0]
-b = l[1]
-count = 0
-result = 0
-while count < m:
-    for i in range(k):
-        result += a
-        count += 1
-        if count == m:
-            break
-    result += b
-    count += 1
+a = l[0]  # 가장 큰 수
+b = l[1]  # 다음으로 큰 수
+count = 0  # 더한 횟수
+result = 0  # 결과
+#   while count < m:
+#       for i in range(k):
+#           result += a
+#           count += 1
+#       if count == m:
+#           break
+#   result += b
+#   count += 1
 
+# 횟수를 일반화하여 푸는 과정
+count = int(m / (k + 1)) * k
+count += m % (k + 1)
+
+result += count * a
+result += (m - count) * b
 print(result)
